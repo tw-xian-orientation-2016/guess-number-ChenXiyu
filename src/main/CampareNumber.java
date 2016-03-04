f316df7ec4ece8d01535da7dd3d75a0eb383ebcc
ChenXiyu <xycinus@gmail.com>
2016-03-04 11:27:53 +0800

    refact srcCode

--- src/main/CampareNumber.java
+++ src/main/CampareNumber.java
@@ -3,14 +3,10 @@ package main;
 public class CampareNumber {
    public String campare(String a , String b){
       int A = 0 , B = 0;
-      for (int j = 0; j < b.length() ; j++) {
-         if (a.indexOf(b.charAt(j)) == -1) {
-            continue;
-         }else {
-            B ++ ;
-         }
-      }
       for (int i = 0; i < a.length() ; i++) {
+         if (a.indexOf(b.charAt(i)) != -1) {
+            B++ ;
+         }
          if(a.charAt(i) == b.charAt(i)){
             A++;
             B--;
